b8c8d60b216b7488384ec6dfd0b807da1c3dc51c
David Dimon <david_dimon@hotmail.com>
2019-05-04 17:37:17 -0300

    fix: added db_type on code

--- models/base.go
+++ models/base.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/joho/godotenv"
 )
 
@@ -32,7 +33,7 @@ func init() {
 	}
 	fmt.Println(dbURI)
 
-	conn, err := gorm.Open("mysql", dbURI)
+	conn, err := gorm.Open(dbType, dbURI)
 	if err != nil {
 		fmt.Print(err)
 	}
